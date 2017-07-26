45c1480de1de390e8a690d243ce755f551d731b3
Jk Jensen <azjkjensen@gmail.com>
2017-07-26 15:49:52 -0600

    Started BeatBox app

---
+++ BeatBox/app/src/main/java/info/jkjensen/beatbox/BeatBoxActivity.kt
@@ -0,0 +1,12 @@
+package info.jkjensen.beatbox
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BeatBoxActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beat_box)
+    }
+}
